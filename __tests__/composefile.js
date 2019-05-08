4a483904c5b45955050c2fd495501ecadbd8cd0b
Joshua Bussdieker <jbussdieker@gmail.com>
2019-05-08 15:40:56 -0700

    Update composefile generator with simple example

--- __tests__/composefile.js
+++ __tests__/composefile.js
@@ -7,10 +7,10 @@ describe('generator-jb-docker-compose:composefile', () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, '../generators/composefile'))
-      .withPrompts({ someAnswer: true });
+      .withOptions({ configVersion: '3.7', serviceName: 'foo', imageName: 'ubuntu' });
   });
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+    assert.file(['docker-compose.yml']);
   });
 });
--- generators/composefile/index.js
+++ generators/composefile/index.js
@@ -1,36 +1,12 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the exquisite ${chalk.red('generator-jb-docker-compose')} generator!`
-      )
-    );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('docker-compose.yml'),
+      this.destinationPath('docker-compose.yml'),
+      { ...this.options }
     );
   }
 
