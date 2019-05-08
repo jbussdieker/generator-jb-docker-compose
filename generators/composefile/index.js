27524e549ac21e01c442de25a3b751898d9eaea3
Joshua Bussdieker <jbussdieker@gmail.com>
2019-05-08 15:30:47 -0700

    Fix lint issue

--- generators/composefile/index.js
+++ generators/composefile/index.js
@@ -7,7 +7,9 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the exquisite ${chalk.red('generator-jb-docker-compose')} generator!`)
+      yosay(
+        `Welcome to the exquisite ${chalk.red('generator-jb-docker-compose')} generator!`
+      )
     );
 
     const prompts = [
